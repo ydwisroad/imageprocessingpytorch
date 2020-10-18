5ebae5bbeb55f81a8914802699d8cda282a11995
PatricWan <wandf12345@163.com>
2020-10-19 06:28:43 +0800

    add two

--- segmentation/SegmentGeneral/toolbox/models/__init__.py
+++ segmentation/SegmentGeneral/toolbox/models/__init__.py
@@ -2,6 +2,9 @@ from toolbox.models.unet import unet
 from toolbox.models.segnet import segnet
 from toolbox.models.linknet import linknet
 from toolbox.models.enet import ENet
+from toolbox.models.FCN import FCN
+from toolbox.models.FusionNet import Fusionnet
+from toolbox.models.DeepLabv3_plus import DeepLabv3_plus
 
 
 def get_model(cfg):
@@ -11,5 +14,8 @@ def get_model(cfg):
         'segnet': segnet,
         'linknet': linknet,
         'enet': ENet,
+        'fcn': FCN,
+        'fusionnet': Fusionnet,
+        'deeplabv3': DeepLabv3_plus
 
     }[cfg['model_name']](n_classes=cfg['n_classes'])
