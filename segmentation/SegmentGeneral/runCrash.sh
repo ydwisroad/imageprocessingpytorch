8901b160e055d9704a1622102c76a0cc9228de5a
shuyin_80@126.com <one@gmail.com>
2020-10-20 20:04:28 +0800

    add more crack

---
+++ segmentation/SegmentGeneral/runCrash.sh
@@ -0,0 +1,5 @@
+python train.py --config=configs/crack_unet.json
+python train.py --config=configs/crack_enet.json
+python train.py --config=configs/crack_fcn.json
+python train.py --config=configs/crack_linknet.json
+python train.py --config=configs/crack_segnet.json
