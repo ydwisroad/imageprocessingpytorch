308d5b4cf01fbd2bb3410d210b2970d53a93e2d0
PatricWan <wandf12345@163.com>
2020-10-31 21:22:10 +0800

    mobilenet

--- classification/ClassificationGeneral/toolbox/models/__init__.py
+++ classification/ClassificationGeneral/toolbox/models/__init__.py
@@ -2,7 +2,7 @@ from toolbox.models.alexnet import AlexNet
 from toolbox.models.googlenet import GoogLeNet
 from toolbox.models.resnet import ResNet
 from toolbox.models.vggnet import VGG
-
+from toolbox.models.mobilenet import MobileNet
 
 def get_model(cfg):
 
@@ -10,5 +10,7 @@ def get_model(cfg):
         'alexnet': AlexNet,
         'googlenet': GoogLeNet,
         'resnet': ResNet,
-        'vgg': VGG
+        'vgg': VGG,
+        'mobilenet': MobileNet
+
     }[cfg['model_name']](num_classes=cfg['num_classes'])
