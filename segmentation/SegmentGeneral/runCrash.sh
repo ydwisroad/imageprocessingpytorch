73169f4aeb325b93bb9322a4993632e6ed960696
patricwan <shuyin_80@126.com>
2020-11-06 18:45:46 +0800

    Delete runCrash.sh

--- segmentation/SegmentGeneral/runCrash.sh
+++
@@ -1,5 +0,0 @@
-python train.py --config=configs/crack_unet.json
-python train.py --config=configs/crack_enet.json
-python train.py --config=configs/crack_fcn.json
-python train.py --config=configs/crack_linknet.json
-python train.py --config=configs/crack_segnet.json
