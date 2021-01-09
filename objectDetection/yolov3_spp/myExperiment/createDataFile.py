c1c6ff75b9ebcb70e7fda1c7fdf058010e347e1e
PatricWan <wandf12345@163.com>
2021-01-09 21:43:51 +0800

    data files

--- objectDetection/yolov3_spp/myExperiment/createDataFile.py
+++ objectDetection/yolov3_spp/myExperiment/createDataFile.py
@@ -4,24 +4,41 @@ import json
 import shutil
 import cv2
 
-root_path = "../../../../data/trafficMini/yolo/"
-save_file_root = "../../../../data/trafficMini/yolo/"
+root_path = "../../../data/TrafficSign/"
 
-line_path = "../../../data/trafficMini/yolo/"
+save_file_root = "../../../data/TrafficSign/"
 
-def main():
+line_path = "../../../data/TrafficSign/"
 
-    list_file = open(os.path.join(save_file_root, 'datatrafficmini.data'), 'w')
+def writeDataFile():
+
+    list_file = open(os.path.join(save_file_root, 'valtraffic.data'), 'w')
 
     trainPath = os.path.join(root_path, 'val/images/')
     for eachFile in os.listdir(trainPath):
         line = os.path.join(line_path, 'val/images/') + eachFile
         print(line)
-        list_file.write('%s ' % (line))
+        list_file.write(line)
+        list_file.write("\n")
 
     list_file.close()
 
+def deleteZeroFile():
+    txtPath = os.path.join(root_path, 'val/labels')
+    count = 0
+    for eachTextFile in os.listdir(txtPath):
+        size = os.path.getsize(txtPath + "/" + eachTextFile)
+        if (size == 0):
+            print(eachTextFile + " 0 ")
+            fileName = eachTextFile.split('.')[0]
+            imgPath = os.path.join(root_path, 'val/images/', fileName + '.jpg')
+            print("imgPath ", imgPath)
+            os.remove(imgPath)
+
+    print("count ", count)
+
 if __name__ == "__main__":
-    main()
+    #deleteZeroFile()
+    writeDataFile()
 
 
