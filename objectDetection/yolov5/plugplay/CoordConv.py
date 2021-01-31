944c208e09f68a35d0c7991a36fc7afc530b9f33
PatricWan <wandf12345@163.com>
2021-01-31 08:02:54 +0800

    modify Coord

--- objectDetection/yolov5/plugplay/CoordConv.py
+++ objectDetection/yolov5/plugplay/CoordConv.py
@@ -57,7 +57,9 @@ class AddCoords(nn.Module):
 
             xx_channel = xx_channel.repeat(batch_size_shape, 1, 1, 1)
             yy_channel = yy_channel.repeat(batch_size_shape, 1, 1, 1)
-
+            
+            device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+            #print("Device ", device) 
             if torch.cuda.is_available and self.use_cuda:
                 input_tensor = input_tensor.cuda()
                 xx_channel = xx_channel.cuda()
@@ -114,7 +116,7 @@ class AddCoords(nn.Module):
 
 class CoordConv1d(conv.Conv1d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True, with_r=False, use_cuda=False):
+                 padding=0, dilation=1, groups=1, bias=True, with_r=False, use_cuda=True):
         super(CoordConv1d, self).__init__(in_channels, out_channels, kernel_size,
                                           stride, padding, dilation, groups, bias)
         self.rank = 1
@@ -136,7 +138,7 @@ class CoordConv1d(conv.Conv1d):
 
 class CoordConv2d(conv.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size=1, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True, with_r=False, use_cuda=False):
+                 padding=0, dilation=1, groups=1, bias=True, with_r=False, use_cuda=True):
         super(CoordConv2d, self).__init__(in_channels, out_channels, kernel_size,
                                           stride, padding, dilation, groups, bias)
         self.rank = 2
@@ -151,7 +153,44 @@ class CoordConv2d(conv.Conv2d):
         :return: CoordConv2d Result
         """
         #print("CoordConv2d ", input_tensor.shape)
-        out = self.addcoords(input_tensor)
+        #out = self.addcoords(input_tensor)
+        
+        batch_size_shape, channel_in_shape, dim_y, dim_x = input_tensor.shape
+        xx_ones = torch.ones([1, 1, 1, dim_x], dtype=torch.int32)
+        yy_ones = torch.ones([1, 1, 1, dim_y], dtype=torch.int32)
+
+        xx_range = torch.arange(dim_y, dtype=torch.int32)
+        yy_range = torch.arange(dim_x, dtype=torch.int32)
+        xx_range = xx_range[None, None, :, None]
+        yy_range = yy_range[None, None, :, None]
+
+        xx_channel = torch.matmul(xx_range, xx_ones)
+        yy_channel = torch.matmul(yy_range, yy_ones)
+
+        # transpose y
+        yy_channel = yy_channel.permute(0, 1, 3, 2)
+
+        xx_channel = xx_channel.float() / (dim_y - 1)
+        yy_channel = yy_channel.float() / (dim_x - 1)
+
+        xx_channel = xx_channel * 2 - 1
+        yy_channel = yy_channel * 2 - 1
+
+        xx_channel = xx_channel.repeat(batch_size_shape, 1, 1, 1)
+        yy_channel = yy_channel.repeat(batch_size_shape, 1, 1, 1)
+        
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        #print("Device ", device) 
+        if torch.cuda.is_available and self.use_cuda:
+            input_tensor = input_tensor.cuda()
+            xx_channel = xx_channel.cuda()
+            yy_channel = yy_channel.cuda()
+        out = torch.cat([input_tensor, xx_channel, yy_channel], dim=1)
+
+        if self.with_r:
+            rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2) + torch.pow(yy_channel - 0.5, 2))
+            out = torch.cat([out, rr], dim=1)
+        
         out = self.conv(out)
 
         return out
