d3774c6e8183e82a6bf884e39a869c6e7b362331
shuyin_80@126.com <one@gmail.com>
2021-02-11 16:09:46 +0800

    to device

--- objectDetection/centernet-pytorch/test.py
+++ objectDetection/centernet-pytorch/test.py
@@ -11,5 +11,6 @@ from nets.centernet import CenterNet_HourglassNet, CenterNet_Resnet50
 if __name__ == "__main__":
     # model = CenterNet_HourglassNet({'hm': 80, 'wh': 2, 'reg':2}).train().cuda()
     # summary(model,(3,128,128))
-    model = CenterNet_Resnet50().train().cuda()
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model = CenterNet_Resnet50().train().to(device)  #.cuda()
     summary(model,(3,512,512))
